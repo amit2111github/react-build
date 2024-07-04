fd044154f1ef50350be0d5118c93cabb13ad233a
amit2111github <amitkumarmandal2111@gmail.com>
2024-07-04 12:55:43 +0530

    removed default exact from route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import { HashRouter, Route, Routes ,useLocation} from 'react-router-dom';
-// import User from "./component/User"
-// import Home from "./component/Home"
 const User = () => {
   console.log(useLocation());
   return <div>user</div>
@@ -18,8 +16,8 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route exact path="/" element= {<Home/>}/>
-        <Route exact path="/user" element = {<User/>}/>
+        <Route  path="/" element= {<Home/>}/>
+        <Route  path="/user" element = {<User/>}/>
       </Routes>
     </HashRouter>
       
